5ce229af0ac485f2a2944a14ef9a10317848040c
Dmitry Kutsenko <d2emonium@gmail.com>
2016-11-27 16:33:08 +0300

    Game base

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,41 @@
 #include <SFML/Graphics.hpp>
+#include <time.h>
 #include "settings.hpp"
+
+struct point
+{int x, y;};
 
 int main()
 {
+    srand(time(0));
+
     // Create the main window
     sf::RenderWindow app(sf::VideoMode(winSize[0], winSize[1]), gameTitle);
+    app.setFramerateLimit(60);
 
     // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("res/cb.bmp"))
+    sf::Texture texture1;
+    if (!texture1.loadFromFile("res/background.png"))
         return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
+
+    sf::Texture texture2;
+    if (!texture2.loadFromFile("res/platform.png"))
+        return EXIT_FAILURE;
+
+    sf::Texture texture3;
+    if (!texture3.loadFromFile("res/doodle.png"))
+        return EXIT_FAILURE;
+
+    sf::Sprite sBackground(texture1);
+    sf::Sprite sPlatform(texture2);
+    sf::Sprite sPers(texture3);
+
+    point plat[20];
+    for(int i=0; i<10; i++)
+    {
+        plat[i].x = rand() % winSize[0];
+        plat[i].y = rand() % winSize[1];
+    }
 
 	// Start the game loop
     while (app.isOpen())
@@ -28,7 +53,13 @@ int main()
         app.clear();
 
         // Draw the sprite
-        app.draw(sprite);
+        app.draw(sBackground);
+        app.draw(sPers);
+        for(int i=0; i<10; i++)
+        {
+            sPlatform.setPosition(plat[i].x, plat[i].y);
+            app.draw(sPlatform);
+        }
 
         // Update the window
         app.display();
